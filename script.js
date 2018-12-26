26ffb17678a22c1783be5f7e4a90da4ba86c6d56
519coding <46140881+519coding@users.noreply.github.com>
2018-12-26 08:58:29 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+/* header scroll effect */
+
+var prevPosition = window.pageYOffset;
+
+window.onscroll = function() {
+
+  var currentPosition = window.pageYOffset;
+
+  if (prevPosition > currentPosition) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-55px";
+  }
+
+  prevPosition = currentPosition;
+}
+
